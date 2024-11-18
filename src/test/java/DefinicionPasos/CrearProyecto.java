a29f14d50182a9a5c44c57e1bcccb05f85e75afc
Ignacio Veliz <101842633+MarKawaii@users.noreply.github.com>
2024-11-17 21:14:05 -0300

    feat: mejoras

--- src/test/java/DefinicionPasos/CrearProyecto.java
+++ src/test/java/DefinicionPasos/CrearProyecto.java
@@ -3,7 +3,6 @@ package DefinicionPasos;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
